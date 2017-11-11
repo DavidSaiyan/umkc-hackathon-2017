301282b257c705520fd3916234cdec6b3d20be51
David Saiyan <dsh9b@mail.umkc.edu>
2017-11-10 21:40:47 -0600

    removed unneeded dependencies

--- Hackathon-Project/app/app.module.js
+++ Hackathon-Project/app/app.module.js
@@ -1,6 +1,6 @@
 ﻿(function () {
     'use strict';
 
-    angular.module('app', ['ngRoute', 'socialLogin'])
+    angular.module('app', ['ngRoute'])
 
 })();
--- Hackathon-Project/app/app.router.js
+++ Hackathon-Project/app/app.router.js
@@ -3,11 +3,9 @@
 
     angular
         .module('app')
-        .config(['$routeProvider', '$logProvider',  '$locationProvider', 'socialProvider',
-            function ($routeProvider, $logProvider, $locationProvider, socialProvider) {
+        .config(['$routeProvider', '$logProvider',  '$locationProvider',
+            function ($routeProvider, $logProvider, $locationProvider) {
                 $logProvider.debugEnabled(true),
-                    socialProvider.setGoogleKey('139914749858-ij8lcqu288a55cgah0gjdtsokbo8mqp0.apps.googleusercontent.com'),
-                    socialProvider.setFbKey({appId: '266289193576013', apiVersion: 'v2.4'}),
                 $routeProvider
                 .when('/home', {
                     templateUrl: 'app/pages/home.controller.html',
